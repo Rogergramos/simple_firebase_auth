af6c4657f1389c3bcc0af24763b7ecf8d8c74458
roger <rogerg.ramos@hotmail.com>
2019-06-11 16:53:18 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'home_page.dart';
+import 'auth.dart';
+import 'login_page.dart';
+
+void main() => runApp(
+  ChangeNotifierProvider<AuthService>(
+    child: MyApp(),
+    builder: (BuildContext context) {
+      return AuthService();
+    },
+  ),
+);
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: FutureBuilder(
+        future: Provider.of<AuthService>(context).getUser(),
+        builder: (context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return snapshot.hasData ? HomePage() : LoginPage();
+          } else {
+            return Container(color: Colors.white);
+          }
+        },
+      ),
+    );
+  }
+}
